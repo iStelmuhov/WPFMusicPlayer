18d75a42389f90f5e1fd27ce049799cc6203cbe9
Стельмухов Игорь Александрович <ishyrovec@gmail.com>
2016-01-23 17:48:52 +0300

    TabControl aded

--- WPFMusicPlayer/ViewModel/MainViewModel.cs
+++ WPFMusicPlayer/ViewModel/MainViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Shapes;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using MaterialDesignThemes.Wpf;
 using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model.Attachments;
@@ -33,51 +34,8 @@ namespace WPFMusicPlayer.ViewModel
         public const ulong APPID = 5233775;
 
         private ApiAuthParams _autorizeAuthParams;
-        private VkApi _vkApi;
+        public VkApi VkApi;
 
-        public const string AudiosPropertyName = "Audios";
-        private ObservableCollection<Audio> _audios = new ObservableCollection<Audio>();
-        public ObservableCollection<Audio> Audios
-        {
-            get
-            {
-                return _audios;
-            }
-
-            set
-            {
-                if (_audios == value)
-                {
-                    return;
-                }
-
-                _audios = value;
-                RaisePropertyChanged(AudiosPropertyName);
-            }
-        }
-
-
-
-        public const string SelectedAudioPropertyName = "SelectedAudio";
-        private AudioFile _selectedAudioFile = new AudioFile(); 
-        public AudioFile SelectedAudio
-        {
-            get
-            {
-                return _selectedAudioFile;
-            }
-
-            set
-            {
-                if (_selectedAudioFile == value)
-                {
-                    return;
-                }
-
-                _selectedAudioFile = value;
-                RaisePropertyChanged(SelectedAudioPropertyName);
-            }
-        }
 
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
@@ -86,118 +44,15 @@ namespace WPFMusicPlayer.ViewModel
         {
             _autorizeAuthParams=new ApiAuthParams();
 
-            _vkApi=new VkApi();
-            
-            
+            VkApi=new VkApi();
+
             _autorizeAuthParams.Login = "shyrovec@rambler.ru";
             _autorizeAuthParams.Password = "Stelmuhov";
             _autorizeAuthParams.ApplicationId = APPID;
             _autorizeAuthParams.Settings = Settings.All;
 
 
-            _vkApi.Authorize(_autorizeAuthParams);
-
-            Audios=new ObservableCollection<Audio>(_vkApi.Audio.Get((ulong)_vkApi.UserId.Value));
-        }
-
-        private childItem FindVisualChild<childItem>(DependencyObject obj)
-          where childItem : DependencyObject
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
-                if (child != null && child is childItem)
-                    return (childItem)child;
-                else
-                {
-                    childItem childOfChild = FindVisualChild<childItem>(child);
-                    if (childOfChild != null)
-                        return childOfChild;
-                }
-            }
-            return null;
-        }
-
-        private RelayCommand _listViewItemChanged;
-        public RelayCommand ListViewItemChanged
-        {
-            get
-            {
-                return _listViewItemChanged
-                    ?? (_listViewItemChanged = new RelayCommand(
-                    () =>
-                    {
-
-
-                        MakeCanvasPictureVisible(Picture.Play, SelectedAudio.VkAudio);
-                        MakeCanvasPictureVisible(Picture.Pause, ((MainWindow)Application.Current.MainWindow).AudiosList.SelectedItem);
-                        SelectedAudio.VkAudio=((MainWindow)Application.Current.MainWindow).AudiosList.SelectedItem as Audio;
-                        SelectedAudio.PlayAudio();
-                    }));
-            }
-        }
-
-        private RelayCommand _listViewItemClick;
-        public RelayCommand ListViewItemClick
-        {
-            get
-            {
-                return _listViewItemClick
-                    ?? (_listViewItemClick = new RelayCommand(
-                    () =>
-                    {
-                        if (SelectedAudio.VkAudio !=
-                            ((MainWindow) Application.Current.MainWindow).AudiosList.SelectedItem)
-                            return;
-
-                        if (SelectedAudio.isPlaying)
-                        {
-                            MakeCanvasPictureVisible(Picture.Play, SelectedAudio.VkAudio);
-                            SelectedAudio.PauseAudio();
-                        }
-                        else
-                        {
-                            MakeCanvasPictureVisible(Picture.Pause, SelectedAudio.VkAudio);
-                            SelectedAudio.PlayAudio();
-                        }
-                    }));
-            }
-        }
-
-        private void MakeCanvasPictureVisible(Picture picture,object listItem)
-        {
-            ListBoxItem listBoxItem =
-                            (ListBoxItem)
-                                (((MainWindow)Application.Current.MainWindow).AudiosList.ItemContainerGenerator
-                                    .ContainerFromItem(
-                                        listItem));
-            if(listBoxItem==null) return;
-            ContentPresenter contentPresenter = FindVisualChild<ContentPresenter>(listBoxItem);
-            DataTemplate dataTemplate = contentPresenter.ContentTemplate;
-
-            Canvas itemCanvas = (Canvas)dataTemplate.FindName("AudioListItemCanvas", contentPresenter);
-
-            Path playFigure = (Path)itemCanvas.FindName("PlayPath");
-            Path pauseFigure = (Path)itemCanvas.FindName("PausePath");
-
-            switch (picture)
-            {
-                case Picture.Pause:
-                {
-                    pauseFigure.Visibility = Visibility.Visible;
-                    playFigure.Visibility=Visibility.Hidden;
-
-                    break;
-                }
-
-                case Picture.Play:
-                {
-                    playFigure.Visibility = Visibility.Visible;
-                    pauseFigure.Visibility = Visibility.Hidden;
-
-                    break;
-                }
-            }
+            VkApi.Authorize(_autorizeAuthParams);
         }
     }
 }
---
+++ WPFMusicPlayer/ViewModel/UserAudioListViewModel.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using VkNet;
+using VkNet.Enums.Filters;
+using VkNet.Model.Attachments;
+using WPFMusicPlayer.Classes;
+using WPFMusicPlayer.Enums;
+using Page = System.Windows.Controls.Page;
+
+namespace WPFMusicPlayer.ViewModel
+{
+    public class UserAudioListViewModel:ViewModelBase
+    {
+        private MainViewModel _mainVm ;
+
+        public const string AudiosPropertyName = "Audios";
+        private ObservableCollection<Audio> _audios = new ObservableCollection<Audio>();
+        public ObservableCollection<Audio> Audios
+        {
+            get
+            {
+                return _audios;
+            }
+
+            set
+            {
+                if (_audios == value)
+                {
+                    return;
+                }
+
+                _audios = value;
+                RaisePropertyChanged(AudiosPropertyName);
+            }
+        }
+
+
+
+        public const string SelectedAudioPropertyName = "SelectedAudio";
+        private AudioFile _selectedAudioFile = new AudioFile();
+        public AudioFile SelectedAudio
+        {
+            get
+            {
+                return _selectedAudioFile;
+            }
+
+            set
+            {
+                if (_selectedAudioFile == value)
+                {
+                    return;
+                }
+
+                _selectedAudioFile = value;
+                RaisePropertyChanged(SelectedAudioPropertyName);
+            }
+        }
+
+        public UserAudioListViewModel()
+        {
+            _mainVm = ((MainViewModel) Application.Current.MainWindow.DataContext);
+            Audios = new ObservableCollection<Audio>(_mainVm.VkApi.Audio.Get((ulong)_mainVm.VkApi.UserId.Value));
+        }
+
+        public UserAudioListViewModel(MainViewModel mvm)
+        {
+            _mainVm = mvm;
+
+            //Audios = new ObservableCollection<Audio>(_mainVm.VkApi.Audio.Get((ulong)_mainVm.VkApi.UserId.Value));
+        }
+
+        private childItem FindVisualChild<childItem>(DependencyObject obj)
+          where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                    return (childItem)child;
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+
+        private RelayCommand _listViewItemChanged;
+        public RelayCommand ListViewItemChanged
+        {
+            get
+            {
+                return _listViewItemChanged
+                    ?? (_listViewItemChanged = new RelayCommand(
+                    () =>
+                    {
+
+
+                        MakeCanvasPictureVisible(Picture.Play, SelectedAudio.VkAudio);
+
+                        MakeCanvasPictureVisible(Picture.Pause, ((UserAudioList)((MainWindow)Application.Current.MainWindow).UserListItem.Content).AudiosList.SelectedItem);
+                        SelectedAudio.VkAudio = ((UserAudioList)((MainWindow)Application.Current.MainWindow).UserListItem.Content).AudiosList.SelectedItem as Audio;
+                        SelectedAudio.PlayAudio();
+                    }));
+            }
+        }
+
+        private RelayCommand _listViewItemClick;
+
+
+
+        public RelayCommand ListViewItemClick
+        {
+            get
+            {
+                return _listViewItemClick
+                    ?? (_listViewItemClick = new RelayCommand(
+                    () =>
+                    {
+                        if (SelectedAudio.VkAudio !=
+                            ((UserAudioList)((MainWindow)Application.Current.MainWindow).UserListItem.Content).AudiosList.SelectedItem)
+                            return;
+
+                        if (SelectedAudio.isPlaying)
+                        {
+                            MakeCanvasPictureVisible(Picture.Play, SelectedAudio.VkAudio);
+                            SelectedAudio.PauseAudio();
+                        }
+                        else
+                        {
+                            MakeCanvasPictureVisible(Picture.Pause, SelectedAudio.VkAudio);
+                            SelectedAudio.PlayAudio();
+                        }
+                    }));
+            }
+        }
+
+        private void MakeCanvasPictureVisible(Picture picture, object listItem)
+        {
+            ListBoxItem listBoxItem =
+                            (ListBoxItem)
+                                (((UserAudioList)((MainWindow)Application.Current.MainWindow).UserListItem.Content).AudiosList.ItemContainerGenerator
+                                    .ContainerFromItem(
+                                        listItem));
+            if (listBoxItem == null) return;
+            ContentPresenter contentPresenter = FindVisualChild<ContentPresenter>(listBoxItem);
+            DataTemplate dataTemplate = contentPresenter.ContentTemplate;
+
+            Canvas itemCanvas = (Canvas)dataTemplate.FindName("AudioListItemCanvas", contentPresenter);
+
+            Path playFigure = (Path)itemCanvas.FindName("PlayPath");
+            Path pauseFigure = (Path)itemCanvas.FindName("PausePath");
+
+            switch (picture)
+            {
+                case Picture.Pause:
+                    {
+                        pauseFigure.Visibility = Visibility.Visible;
+                        playFigure.Visibility = Visibility.Hidden;
+
+                        break;
+                    }
+
+                case Picture.Play:
+                    {
+                        playFigure.Visibility = Visibility.Visible;
+                        pauseFigure.Visibility = Visibility.Hidden;
+
+                        break;
+                    }
+            }
+        }
+
+    }
+}
